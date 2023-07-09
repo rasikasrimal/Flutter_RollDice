183c311a6bfa24dce4aa924b4c4d9ad67aaa88b7
Rasika srimal <77890327+rasikasrimal@users.noreply.github.com>
2023-07-09 08:55:06 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ void main() {
       home: Scaffold(
         body: GradientContainer(
           Color.fromARGB(255, 33, 5, 109),
-          Color.fromARGB(255, 68, 21, 149),
+          Color.fromARGB(255, 68, 21, 150),
         ),
       ),
     ),
